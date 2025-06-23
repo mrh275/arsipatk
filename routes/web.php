d2e7d60d886ec0c046786db02f62065ca1875de7
Muhamad Ramdani Hidayatullah <muhamadramdani.tmkm@gmail.com>
2025-06-23 17:44:03 +0700

    create new route for dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [LoginController::class, 'index']);
+Route::get('admin/dashboard', [DashboardController::class, 'index']);
