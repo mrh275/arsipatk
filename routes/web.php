fb2db477b95d0e551cf031ac4bad5da8acc816e6
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 21:44:48 +0700

    create new route for permintaan and penerimaan barang; default view, tambah permintaan view and store pertminaan request

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\BarangController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PenerimaanController;
+use App\Http\Controllers\PermintaanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LoginController::class, 'index']);
 Route::get('admin/dashboard', [DashboardController::class, 'index']);
-Route::get('admin/master/kategori', [KategoriController::class, 'index']);
 
+// Route untuk Master Data
+Route::get('admin/master/kategori', [KategoriController::class, 'index']);
 Route::get('admin/master/barang', [BarangController::class, 'index']);
+
+// Route untuk Transaksi
+Route::get('admin/transaksi/data-permintaan', [PermintaanController::class, 'index']);
+Route::get('admin/transaksi/tambah-permintaan', [PermintaanController::class, 'tambahPermintaan']);
+Route::post('admin/transaksi/tambah-permintaan', [PermintaanController::class, 'simpanPermintaan']);
+Route::get('admin/transaksi/data-penerimaan', [PenerimaanController::class, 'index']);
