1e0a1e41be1d36f3ac2c0e4c29de92d818058601
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 20:41:06 +0700

    create new route for barang default view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LoginController;
@@ -19,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LoginController::class, 'index']);
 Route::get('admin/dashboard', [DashboardController::class, 'index']);
 Route::get('admin/master/kategori', [KategoriController::class, 'index']);
+
+Route::get('admin/master/barang', [BarangController::class, 'index']);
