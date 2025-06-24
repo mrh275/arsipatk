7d88bc849dedbbbb843be4e1e403d6a19e7b09c5
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 21:46:01 +0700

    add new column fields for stok barang

--- resources/views/admin/barang/data-barang.blade.php
+++ resources/views/admin/barang/data-barang.blade.php
@@ -37,6 +37,8 @@
                                             <th>ID Barang</th>
                                             <th>Nama Kategori</th>
                                             <th>Nama Barang</th>
+                                            <th>Stok Barang</th>
+                                            <th>Satuan Barang</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
@@ -47,6 +49,8 @@
                                                 <td>{{ $barang->id_barang }}</td>
                                                 <td>{{ $barang->kategori->nama_kategori }}</td>
                                                 <td>{{ $barang->nama_barang }}</td>
+                                                <td>{{ $barang->stok_barang }}</td>
+                                                <td>{{ $barang->satuan_barang }}</td>
                                                 <td>
                                                     <a href="#" class="btn btn-warning btn-sm">
                                                         Edit
