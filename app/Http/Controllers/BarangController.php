475b87c9030b6695ee5766dea7c6000ff68f16b5
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 20:43:04 +0700

    create new method for default view page for data barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
 use Illuminate\Http\Request;
 
 class BarangController extends Controller
 {
-    //
+    public function index()
+    {
+        $dataBarang = Barang::with('kategori')->get();
+        $data = [
+            'title' => 'Data Barang',
+            'dropdown' => 'master',
+            'active' => 'barang',
+            'dataBarang' => $dataBarang,
+            'hasDatatable' => '1',
+        ];
+
+        return view('admin.barang.data-barang', $data);
+    }
 }
