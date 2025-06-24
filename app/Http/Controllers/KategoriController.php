37dd3fbab4cff9c019ed1469645ee093af850429
Muhamad Ramdani Hidayatullah <muhamadramdani.tmkm@gmail.com>
2025-06-24 17:59:11 +0700

    create new method for kategori default page

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    //
+    public function index()
+    {
+        $dataKategori = Kategori::all();
+        $data = [
+            'title' => 'Kategori',
+            'active' => 'kategori',
+            'dataKategori' => $dataKategori,
+            'hasDatatable' => '1',
+        ];
+        return view('admin.kategori.data-kategori', $data);
+    }
 }
