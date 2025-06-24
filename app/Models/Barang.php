affb473057e8613846b03cbc22e3bc321aeb7994
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 20:41:47 +0700

    create new method for relation from table barang to table kategori

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -9,4 +9,13 @@ class Barang extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function kategori()
+    {
+        // Argumen pertama: Model target relasi (Kategori::class)
+        // Argumen kedua: Foreign key di model Barang ('id_kategori')
+        // Argumen ketiga (opsional): Local key di model Kategori (default: primary key model target)
+        // Jika primary key Kategori juga 'id_kategori', maka argumen ketiga bisa dihilangkan.
+        return $this->belongsTo(Kategori::class, 'id_kategori', 'id_kategori');
+    }
 }
