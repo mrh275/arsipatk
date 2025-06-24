932d1eb68834d74ab8a89b601e8562e5bc6fd3e4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 20:43:42 +0700

    create new viewpage with datatable for default view page for data barang

---
+++ resources/views/admin/barang/data-barang.blade.php
@@ -0,0 +1,88 @@
+@extends('templates.admin-layout')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Data Barang</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                            <li class="breadcrumb-item"><a href="#">Master</a></li>
+                            <li class="breadcrumb-item active">Barang</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>ID Barang</th>
+                                            <th>Nama Kategori</th>
+                                            <th>Nama Barang</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($dataBarang as $barang)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $barang->id_barang }}</td>
+                                                <td>{{ $barang->kategori->nama_kategori }}</td>
+                                                <td>{{ $barang->nama_barang }}</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-warning btn-sm">
+                                                        Edit
+                                                    </a>
+                                                    <a href="#" class="btn btn-danger btn-sm">
+                                                        Hapus
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
+
+@push('scripts')
+    <script>
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+            });
+        });
+    </script>
+@endpush
