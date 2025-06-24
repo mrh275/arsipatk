1fc82c9743c358dde9279f02bcf917dcbbdf9274
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 21:41:45 +0700

    create new instances for penerimaan barang

---
+++ database/migrations/2025_06_24_134745_create_penerimaans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penerimaans', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_penerimaan')->unique();
+            $table->date('tanggal_penerimaan');
+            $table->string('id_barang');
+            $table->integer('jumlah')->unsigned();
+            $table->string('satuan_barang');
+            $table->string('keterangan')->nullable();
+            $table->string('status')->default('pending'); // pending, approved, rejected
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penerimaans');
+    }
+};
