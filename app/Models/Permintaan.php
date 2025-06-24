716e9221814b2457972a5eebf1f83eaf9500cc2c
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 21:43:14 +0700

    create db relation from permintaan barang to barang

---
+++ app/Models/Permintaan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permintaan extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function barang()
+    {
+        // Argumen pertama: Model target relasi (Barang::class)
+        // Argumen kedua: Foreign key di model Permintaan ('id_barang')
+        // Argumen ketiga (opsional): Local key di model Barang (default: primary key model target)
+        // Jika primary key Barang juga 'id_barang', maka argumen ketiga bisa dihilangkan.
+        return $this->belongsTo(Barang::class, 'id_barang', 'id_barang');
+    }
+}
