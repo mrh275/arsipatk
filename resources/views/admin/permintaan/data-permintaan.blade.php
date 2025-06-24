9137feb728b8878c92568da979bdfd0a32f9d23d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-24 21:46:31 +0700

    create data permintaan default view with datatable

---
+++ resources/views/admin/permintaan/data-permintaan.blade.php
@@ -0,0 +1,106 @@
+@extends('templates.admin-layout')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Data Barang</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                            <li class="breadcrumb-item"><a href="#">Master</a></li>
+                            <li class="breadcrumb-item active">Barang</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>ID Permintaan</th>
+                                            <th>Issued by</th>
+                                            <th>Nama Barang</th>
+                                            <th>Jumlah Permintaan</th>
+                                            <th>Satuan</th>
+                                            <th>Tanggal Permintaan</th>
+                                            <th>Status Permintaan</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($dataPermintaan as $permintaan)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $permintaan->id_permintaan }}</td>
+                                                <td>{{ $permintaan->issued_by }}</td>
+                                                <td>{{ $permintaan->barang->nama_barang }}</td>
+                                                <td>{{ $permintaan->jumlah_permintaan }}</td>
+                                                <td>{{ $permintaan->barang->satuan_barang }}</td>
+                                                <td>{{ $permintaan->tanggal_permintaan }}</td>
+                                                <td>{{ $permintaan->status_permintaan }}</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-warning btn-sm">
+                                                        Edit
+                                                    </a>
+                                                    <a href="#" class="btn btn-danger btn-sm">
+                                                        Hapus
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
+
+@push('scripts')
+    <script>
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": [{
+                    text: '<i class="fas fa-plus"></i> Tambah Permintaan',
+                    className: 'btn-primary',
+                    init: function(api, node, config) {
+                        $(node).removeClass('btn-secondary');
+                    },
+                    action: function(e, dt, node, config) {
+                        window.location.href = "{{ url('admin/transaksi/tambah-permintaan') }}";
+                    }
+                }]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        });
+    </script>
+@endpush
