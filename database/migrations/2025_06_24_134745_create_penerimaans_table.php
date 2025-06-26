e7d458b74f6e6f045d192426737ef01ee36876bd
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-26 19:28:27 +0700

    modified and remove some lines

--- database/migrations/2025_06_24_134745_create_penerimaans_table.php
+++ database/migrations/2025_06_24_134745_create_penerimaans_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
     {
         Schema::create('penerimaans', function (Blueprint $table) {
             $table->id();
-            $table->string('kode_penerimaan')->unique();
+            $table->string('id_penerimaan')->unique();
             $table->date('tanggal_penerimaan');
             $table->string('id_barang');
             $table->integer('jumlah')->unsigned();
             $table->string('satuan_barang');
             $table->string('keterangan')->nullable();
-            $table->string('status')->default('pending'); // pending, approved, rejected
             $table->timestamps();
         });
     }
