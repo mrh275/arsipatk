32dc4f1bff943aa31691ee567502535fcfba37ca
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-26 19:27:46 +0700

    create method for relation to table barang

--- app/Models/Penerimaan.php
+++ app/Models/Penerimaan.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Penerimaan extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function barang()
+    {
+        // Argumen pertama: Model target relasi (Barang::class)
+        // Argumen kedua: Foreign key di model Permintaan ('id_barang')
+        // Argumen ketiga (opsional): Local key di model Barang (default: primary key model target)
+        // Jika primary key Barang juga 'id_barang', maka argumen ketiga bisa dihilangkan.
+        return $this->belongsTo(Barang::class, 'id_barang', 'id_barang');
+    }
 }
