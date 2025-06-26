542d8aff429cd58f32b0a1611cafa2fc697232af
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-26 20:13:39 +0700

    add modal for edit data kategori; add flash alert when session has flashdata; modified delete button; modified edit button; add event for show alert when delete kategoril add event for edit specified kategori;

--- resources/views/admin/kategori/data-kategori.blade.php
+++ resources/views/admin/kategori/data-kategori.blade.php
@@ -18,6 +18,22 @@
                         </ol>
                     </div>
                 </div>
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
+                @if (session('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('error') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
             </div><!-- /.container-fluid -->
         </section>
 
@@ -46,10 +62,10 @@
                                                 <td>{{ $kategori->id_kategori }}</td>
                                                 <td>{{ $kategori->nama_kategori }}</td>
                                                 <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">
+                                                    <button type="button" id="editKategori" value="{{ $kategori->id_kategori }}" class="btn btn-warning btn-sm">
                                                         Edit
-                                                    </a>
-                                                    <a href="#" class="btn btn-danger btn-sm">
+                                                    </button>
+                                                    <a href="{{ url('admin/master/kategori/hapus') . '/' . $kategori->id_kategori }}" class="btn btn-danger btn-sm">
                                                         Hapus
                                                     </a>
                                                 </td>
@@ -71,6 +87,41 @@
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
+
+    {{-- Edit Modal --}}
+    <div class="modal fade" id="editModal">
+        <div class="modal-dialog">
+            <form action="{{ url('admin/master/kategori/update') }}" method="post">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Edit Kategori</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        @csrf
+                        <div class="form-group">
+                            <label for="id_kategori">ID Kategori</label>
+                            <input type="hidden" class="form-control" id="id_kategori" name="id_kategori" required>
+                            <input type="text" class="form-control" id="id_kategori_preview" name="id_kategori_preview" disabled="disabled" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="nama_kategori">Nama Kategori</label>
+                            <input type="text" name="nama_kategori" id="nama_kategori" class="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </div>
+            </form>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 @endsection
 
 @push('scripts')
@@ -80,6 +131,57 @@
                 "responsive": true,
                 "lengthChange": false,
                 "autoWidth": false,
+                "buttons": [{
+                    text: '<i class="fas fa-plus"></i> Tambah Kategori',
+                    className: 'btn-primary',
+                    init: function(api, node, config) {
+                        $(node).removeClass('btn-secondary');
+                    },
+                    action: function(e, dt, node, config) {
+                        window.location.href = "{{ url('admin/master/tambah-kategori') }}";
+                    }
+                }]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        });
+        $(document).ready(function() {
+            $('#example1').on('click', '.btn-danger', function(e) {
+                e.preventDefault();
+                const url = $(this).attr('href');
+                Swal.fire({
+                    title: 'Hapus Kategori',
+                    text: "Apakah Anda yakin ingin menghapus data ini?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Ya, Hapus!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = url;
+                    }
+                });
+            });
+
+            $('#example1').on('click', '#editKategori', function() {
+                const idKategori = $(this).val();
+                // Fetch data for the selected penerimaan
+                $.ajax({
+                    url: "{{ url('admin/master/kategori') }}/" + idKategori,
+                    method: 'GET',
+                    success: function(response) {
+                        $('#id_kategori').val(response.data.id_kategori);
+                        $('#id_kategori_preview').val(response.data.id_kategori);
+                        $('#nama_kategori').val(response.data.nama_kategori);
+                        $('#editModal').modal('show');
+                    },
+                    error: function(response) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: response.error
+                        });
+                    }
+                });
             });
         });
     </script>
