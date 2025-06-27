3658bf0168ea4e6798094a3d8dca766899931142
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-27 12:30:29 +0700

    create controller for laporan

---
+++ app/Http/Controllers/LaporanController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class LaporanController extends Controller
+{
+    public function laporanPersediaan()
+    {
+
+        $data = [
+            'title' => 'Laporan Persediaan',
+            'dropdown' => 'laporan',
+            'active' => 'persediaan',
+            'hasDatatable' => '0'
+        ];
+
+        return view('admin.laporan.persediaan', $data);
+    }
+
+    public function cetakLaporanPersediaan(Request $request)
+    {
+        // Logika untuk mencetak laporan persediaan
+        // Misalnya, mengambil data dari database berdasarkan filter yang diberikan
+
+        $format = $request->input('format_laporan');
+
+        $dataBarang = Barang::with('kategori')->get(); // Ambil semua data barang
+
+        dd($dataBarang); // Debugging, bisa dihapus setelah selesai
+        // Proses data dan cetak laporan sesuai format yang dipilih (PDF/Excel)
+        $pdf = PDF::loadView('admin.laporan.export.persediaan-pdf', [
+            'dataBarang' => $dataBarang,
+            'format' => $format
+        ]);
+
+        return response()->json(['message' => 'Laporan berhasil dicetak']);
+    }
+}
