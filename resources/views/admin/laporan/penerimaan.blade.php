93525b4f2a0d56295afd11e804b97904f2542959
Muhamad Ramdani Hidayatullah <muhamadramdani.tmkm@gmail.com>
2025-06-27 13:15:26 +0700

    create view page for form export penerimaan

---
+++ resources/views/admin/laporan/penerimaan.blade.php
@@ -0,0 +1,92 @@
+@extends('templates.admin-layout')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Laporan Penerimaan</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                            <li class="breadcrumb-item"><a href="#">Laporam</a></li>
+                            <li class="breadcrumb-item active">Penerimaan</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <form action="{{ url('admin/laporan/penerimaan') }}" method="post">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="format_laporan">Format Laporan</label>
+                                        <select name="format_laporan" class="form-control" id="format_laporan">
+                                            <option value="">Pilih :</option>
+                                            <option value="pdf">PDF</option>
+                                            <option value="excel">Excel</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="periode_tahun">Periode tahun</label>
+                                        <select name="periode_tahun" class="form-control" id="periode_tahun">
+                                            <option value="">Pilih :</option>
+                                            <option value="2025">2025</option>
+                                            <option value="2024">2024</option>
+                                            <option value="2023">2023</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="periode_bulan">Periode Bulan</label>
+                                        <select name="periode_bulan" class="form-control" id="periode_bulan">
+                                            <option value="">Pilih :</option>
+                                            <option value="all">All</option>
+                                            <option value="01">Januari</option>
+                                            <option value="02">Februari</option>
+                                            <option value="03">Maret</option>
+                                            <option value="04">April</option>
+                                            <option value="05">Mei</option>
+                                            <option value="06">Juni</option>
+                                            <option value="07">Juli</option>
+                                            <option value="08">Agustus</option>
+                                            <option value="09">September</option>
+                                            <option value="10">Oktober</option>
+                                            <option value="11">November</option>
+                                            <option value="12">Desember</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
+
+@push('scripts')
+@endpush
