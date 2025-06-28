25dc03bbe8583f6f36df0593a3a6ff513628aa20
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-28 13:33:19 +0700

    modified export method from view to collection

--- app/Exports/PenerimaanBarangExport.php
+++ app/Exports/PenerimaanBarangExport.php
@@ -3,19 +3,68 @@
 namespace App\Exports;
 
 use App\Models\Penerimaan;
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Carbon\Carbon;
 
-class PenerimaanBarangExport implements FromView, ShouldAutoSize
+class PenerimaanBarangExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
     /**
      * @return \Illuminate\Support\Collection
      */
-    public function view(): View
+
+    protected $periodeTahun;
+    protected $periodeBulan;
+
+    public function __construct($periodeTahun = null, $periodeBulan = null)
     {
-        return view('admin.laporan.export.penerimaan-excel', [
-            'dataPenerimaan' => Penerimaan::with('barang')->get() // Ambil semua data barang dengan relasi kategori
-        ]);
+        $this->periodeTahun = $periodeTahun;
+        $this->periodeBulan = $periodeBulan;
+    }
+
+    public function collection()
+    {
+        // Ambil data penerimaan
+        $query = Penerimaan::with('barang');
+
+        // Filter data berdasarkan periode tahun dan bulan
+        if ($this->periodeTahun) {
+            $query->whereYear('tanggal_penerimaan', $this->periodeTahun);
+        }
+
+        if ($this->periodeBulan && $this->periodeBulan !== 'all') {
+            $query->whereMonth('tanggal_penerimaan', $this->periodeBulan);
+        }
+
+        $dataPenerimaan = $query->get();
+
+        // Map data untuk disesuaikan dengan kolom yang diinginkan
+        return $dataPenerimaan->map(function ($penerimaan) {
+            // Format tanggal menggunakan Carbon
+            $tanggalFormatted = Carbon::parse($penerimaan->tanggal_penerimaan)->locale('id')->isoFormat('D MMMM YYYY');
+
+            return [
+                'ID Penerimaan' => $penerimaan->id_penerimaan,
+                'Nama Barang' => optional($penerimaan->barang)->nama_barang,
+                'Jumlah' => $penerimaan->jumlah,
+                'Satuan' => $penerimaan->satuan_barang,
+                'Tanggal Penerimaan' => $tanggalFormatted,
+            ];
+        });
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'ID Penerimaan',
+            'Nama Barang',
+            'Jumlah',
+            'Satuan',
+            'Tanggal Penerimaan',
+        ];
     }
 }
--- app/Exports/PermintaanBarangExport.php
+++ app/Exports/PermintaanBarangExport.php
@@ -3,21 +3,69 @@
 namespace App\Exports;
 
 use App\Models\Permintaan;
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Support\Carbon;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class PermintaanBarangExport implements FromView, ShouldAutoSize
+class PermintaanBarangExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
     /**
      * @return \Illuminate\Support\Collection
      */
-    public function view(): View
-    {
-        $dataPermintaan = Permintaan::with(['barang'])->get(); // Ambil semua data permintaan
 
-        return view('admin.laporan.export.permintaan-excel', [
-            'dataPermintaan' => $dataPermintaan,
-        ]);
+    protected $periodeTahun;
+    protected $periodeBulan;
+
+    public function __construct($periodeTahun = null, $periodeBulan = null)
+    {
+        $this->periodeTahun = $periodeTahun;
+        $this->periodeBulan = $periodeBulan;
+    }
+
+    public function collection()
+    {
+        // Ambil data permintaan
+        $query = Permintaan::with('barang');
+
+        // Filter data berdasarkan periode tahun dan bulan
+        if ($this->periodeTahun) {
+            $query->whereYear('tanggal_permintaan', $this->periodeTahun);
+        }
+
+        if ($this->periodeBulan && $this->periodeBulan !== 'all') {
+            $query->whereMonth('tanggal_permintaan', $this->periodeBulan);
+        }
+
+        $dataPermintaan = $query->get();
+
+        // Map data untuk disesuaikan dengan kolom yang Anda inginkan
+        return $dataPermintaan->map(function ($permintaan) {
+            // Format tanggal menggunakan Carbon
+            $tanggalFormatted = Carbon::parse($permintaan->tanggal_permintaan)->locale('id')->isoFormat('D MMMM YYYY');
+            return [
+                'ID Permintaan' => $permintaan->id_permintaan,
+                'Issued by' => $permintaan->issued_by,
+                'Nama Barang' => optional($permintaan->barang)->nama_barang, // Menggunakan optional() untuk menghindari error jika relasi 'barang' null
+                'Jumlah Permintaan' => $permintaan->jumlah_permintaan,
+                'Satuan' => optional($permintaan->barang)->satuan_barang, // Menggunakan optional() untuk menghindari error jika relasi 'barang' null
+                'Tanggal Permintaan' => $tanggalFormatted,
+            ];
+        });
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'ID Permintaan',
+            'Issued by',
+            'Nama Barang',
+            'Jumlah Permintaan',
+            'Satuan',
+            'Tanggal Permintaan',
+        ];
     }
 }
