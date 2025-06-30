f0fed00043e426b2e10193fc9393c1da756977ad
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-30 19:23:51 +0700

    added blade directive for stack css

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="{{ asset('assets') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('assets') }}/dist/css/adminlte.min.css">
+    @stack('styles')
 </head>
 
 <body>
