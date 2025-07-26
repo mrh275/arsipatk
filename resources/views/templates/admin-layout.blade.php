c9813b052db3ff70b7abf836871e59558f7eb32c
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-07-26 17:40:53 +0700

    added css assets for select2

--- resources/views/templates/admin-layout.blade.php
+++ resources/views/templates/admin-layout.blade.php
@@ -34,6 +34,10 @@
         <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.3/css/buttons.dataTables.css">
     @endif
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.22.1/dist/sweetalert2.min.css">
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
