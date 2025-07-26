6665585e522cc81cc74dcdf593f6ce043e24a2b8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-07-26 18:29:23 +0700

    added logout menu

--- resources/views/templates/admin-sidebar.blade.php
+++ resources/views/templates/admin-sidebar.blade.php
@@ -110,7 +110,14 @@
                         </li>
                     </ul>
                 </li>
-
+                <li class="nav-item">
+                    <a href="{{ url('logout') }}" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Logout
+                        </p>
+                    </a>
+                </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
