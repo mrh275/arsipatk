2af97224ef6bfcc7045857848166167530bbcd79
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-07-26 17:40:42 +0700

    change user name on sidebar with name from session

--- resources/views/templates/admin-sidebar.blade.php
+++ resources/views/templates/admin-sidebar.blade.php
@@ -13,7 +13,7 @@
                 <img src="{{ asset('assets') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Administrator</a>
+                <a href="#" class="d-block">{{ session('name') }}</a>
             </div>
         </div>
 
